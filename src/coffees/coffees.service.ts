0b9ef786542b9c820e28e9ba7a2004ea199df095
Gerardo Ramirez Serapio <geramirez@blim.com>
2022-05-17 21:09:37 -0500

    Chapter 1

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,51 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll(): Coffee[] {
+    return this.coffees;
+  }
+
+  findOne(id: string): Coffee {
+    const coffee = this.coffees.find((coffee) => coffee.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return coffee;
+  }
+
+  create(createCoffeeDto: any): void {
+    this.coffees.push(createCoffeeDto);
+    return createCoffeeDto;
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      console.log(updateCoffeeDto);
+      // update the existing entity
+    }
+  }
+
+  remove(id: string): void {
+    const coffeeIndex = this.coffees.findIndex((coffee) => coffee.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
